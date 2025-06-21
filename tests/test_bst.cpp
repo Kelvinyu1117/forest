5690867a9e0bf3ce90fceeac5e83987a3094ce05
Kelvin Wu <kelvinyu1117@gmail.com>
2025-06-21 22:28:13 +1000

    feat: implement insert, find, iterator for naive binary search tree

--- tests/test_bst.cpp
+++ tests/test_bst.cpp
@@ -1,14 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
-
 #include <cstdint>
+#include "bst.h"
+#include <iostream>
 
-uint32_t factorial( uint32_t number ) {
-    return number <= 1 ? number : factorial(number-1) * number;
-}
-
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( factorial( 1) == 1 );
-    REQUIRE( factorial( 2) == 2 );
-    REQUIRE( factorial( 3) == 6 );
-    REQUIRE( factorial(10) == 3'628'800 );
+TEST_CASE( "LinkedBinarySearchTree", "[traversal]" ) {
+    forest::LinkedBinarySearchTree<int> bst;
+    // bst.insert(5);
+    // bst.insert(2);
+    // bst.insert(10);
+    // for(auto i: bst){
+    //     std::cout << i << '\n';
+    // }
 }
